a45983627d6a81a8465a33df3e85a54e95e35709
Konrad <khaase@uwo.ca>
2019-01-09 21:24:55 -0500

    Made options page better, testing highlighting options on sample ad.

--- options.js
+++ options.js
@@ -40,3 +40,67 @@ function makeUL(array) {
     // Finally, return the constructed list:
     return list;
 }
+
+
+/*
+ * Listeners for checkbox changes.
+ *
+ * Pattern from : https://stackoverflow.com/questions/14544104/checkbox-check-event-listener
+ */
+var checkbox = document.querySelector("input[name=grayscale]");
+
+//Grayscale
+checkbox.addEventListener('change', function () {
+    var element = document.getElementById("exampleAd");
+    if (this.checked) {
+        // Checkbox is checked..
+        element.classList.add("grayscale");
+    } else {
+        // Checkbox is not checked..
+        element.classList.remove("grayscale");
+    }
+});
+
+//Color
+checkbox = document.querySelector("input[name=color]");
+checkbox.addEventListener('change', function () {
+    var element = document.getElementById("exampleAd");
+    if (this.checked) {
+        // Checkbox is checked..
+        element.classList.add("color");
+    } else {
+        // Checkbox is not checked..
+        element.classList.remove("color");
+    }
+});
+
+//Border
+checkbox = document.querySelector("input[name=border]");
+checkbox.addEventListener('change', function () {
+    var element = document.getElementById("exampleAd");
+    if (this.checked) {
+        // Checkbox is checked..
+        element.classList.add("border");
+    } else {
+        // Checkbox is not checked..
+        element.classList.remove("border");
+    }
+});
+
+//Label
+checkbox = document.querySelector("input[name=label]");
+checkbox.addEventListener('change', function () {
+    var element = document.getElementById("exampleAd");
+    if (this.checked) {
+        // Checkbox is checked..
+        element.classList.add("label");
+    } else {
+        // Checkbox is not checked..
+        element.classList.remove("label");
+    }
+});
+
+
+/*
+ * To make Label and color work properly see how they overlay stuff here: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_image_overlay_fade
+ */
