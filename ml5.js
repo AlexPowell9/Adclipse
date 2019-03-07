053ae67b14d4d597ac85a39ac65c5f517f43e4bf
Christian Yores <christianyores@gmail.com>
2019-03-06 22:44:53 -0500

    ml5 actual loads and analyzed content. lots of false positives

--- ml5.js
+++ ml5.js
@@ -6,8 +6,46 @@
 
 let ML5 = {};
 var adsFound = 0;
+var features;
+var classifier;
+
+function ml5Initialize() {
+    return new Promise(function(resolve, reject) {
+        console.log("Loading Feature Extractor...");
+        features = ml5.featureExtractor('MobileNet', () => {
+            /*
+             * This is a weird thing that may or may not have been fixed in new version. With more than 2 classes it was refusing to pick up more classes.
+             * https://github.com/ml5js/ml5-library/issues/164
+             */
+            features.numClasses = 3;
+            console.log("Loading Classifier...");
+            classifier = features.classification();
+            console.log("Loading Model...");
+            var t0 = performance.now();
+            //https://developer.chrome.com/extensions/content_scripts
+            classifier.load(chrome.runtime.getURL("external/ml5/model.json"), () => {
+                var t1 = performance.now();
+                console.log("Model Loaded in " + (t1 - t0).toFixed(2) + " ms.");
+                resolve("Done init ML5");
+            });
+        });
+    });
+}
+
+//Load Model
+// function ml5LoadModel() {
+//     console.log("Loading Model...");
+//     var t0 = performance.now();
+//     //https://developer.chrome.com/extensions/content_scripts
+//     classifier.load(chrome.runtime.getURL("external/ml5/model.json"), () => {
+//         var t1 = performance.now();
+//         console.log("Model Loaded in " + (t1 - t0).toFixed(2) + " ms.");
+//     });
+// }
 
 ML5.process = async function (containers) {
+    await ml5Initialize();
+    print("Done loading ML5");
     var adContainers = [];
     var allCanvases = [];
     var canvasPromises = convertToCanvases(containers);
@@ -19,21 +57,23 @@ ML5.process = async function (containers) {
     });
 
 
-    // wait for tesseract to analyze the canvases/containers
-    await Promise.all(processImages(allCanvases)).then(results => {
-        results.forEach(function (result, index) {
-            console.log('Tesseract Result:', tesseractResult(result));
-            if (result.text.includes("PROMOTED") ||
-                result.text.includes("PRDMDVED") ||
-                result.text.includes("FROMOTED") ||
-                result.text.includes("ADVERTISEMENT") ||
-                result.text.includes("Anvzmsmm")
-            ) {
-                adContainers.push(containers[index]);
-            }
-        });
+    // wait for ml5 to analyze the canvases/containers
+    await Promise.all(processImages(allCanvases, containers)).then(results => {
+        print('Results of all', results);
+        // results.forEach(function (result, index) {
+        //     console.log('Tesseract Result:', tesseractResult(result));
+        //     if (result.text.includes("PROMOTED") ||
+        //         result.text.includes("PRDMDVED") ||
+        //         result.text.includes("FROMOTED") ||
+        //         result.text.includes("ADVERTISEMENT") ||
+        //         result.text.includes("Anvzmsmm")
+        //     ) {
+        //         adContainers.push(containers[index]);
+        //     }
+        // });
     });
 
+    // return processImages(allCanvases, containers);
 
 
     // return the containers that ml5 thinks have ads
@@ -65,24 +105,53 @@ function convertToCanvases(containers) {
 
 
 /*
- * OCR Images
- * Takes in canvases, converts them to image data, puts image data through Tesseract
- * Returns: Tesseract promises
+ * ML5 Images
+ * Takes in canvases, converts them to image data, puts image data through Ml5
+ * Returns: 
  */
 
-function processImages(canvases) {
+function processImages(canvases, containers) {
     let promises = [];
-    canvases.forEach(canvas => {
-        let ctx = canvas.getContext('2d');
-        var expanded = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        console.log(expanded);
-        chrome.runtime.sendMessage({
-            classifyImage: expanded
-        }, function (response) {
-            console.log("Response Recieved: " + response.response);
-        });
 
-        promises.push(Tesseract.recognize(expanded));
+    canvases.forEach(function(canvas, index) {
+        let img = new Image();
+        img.src = canvas.toDataURL();
+        img.crossOrigin = "anonymous";
+        img.width = 224;
+        img.height = 224;
+        promises.push(
+            classifier.classify(img, function (err, results) {
+                if (err) {
+                    console.log("ML5 Classify Error" + err);
+                } else {
+                    console.log("ML5 Result:", results, img);
+                    if (results === "Advertisement") {
+                        console.log('adcontainer', containers[index]);
+                        containers[index].classList.add('adclipseGrayscale');
+                    }
+                }
+            })
+        );
+
+        // promises.push(new Promise(function(resolve, reject) {
+        //     let img = new Image();
+        //     img.src = canvas.toDataURL();
+        //     img.crossOrigin = "anonymous";
+        //     img.width = 224;
+        //     img.height = 224;
+        //     classifier.classify(img, function (err, results) {
+        //         if (err) {
+        //             console.log("ML5 Classify Error" + err);
+        //             resolve(err);
+        //         } else {
+        //             console.log("ML5 Result: " + results, canvas);
+        //             if (results === "Advertisement") {
+        //                 promises.push(containers[index]);
+        //             }
+        //             resolve(results);
+        //         }
+        //     });
+        // }));
     });
     return promises;
 }
