3bcb1362845715eb3c1b7af1ce38721a3a253371
Christian Yores <christianyores@gmail.com>
2019-03-11 20:23:28 -0400

    Brought new screenshots to OCR, kept h2c commented

--- content.js
+++ content.js
@@ -58,8 +58,8 @@ chrome.storage.local.get("whitelist", function (returnedStorage) {
          * TODO: pull from options to check which one is enabled.
          */
         // console.log("Called OCR");
-        // evaluateContainers('ocr');
-        evaluateContainers('ml5');
+        evaluateContainers('ocr');
+        // evaluateContainers('ml5');
         //Run ML5 on scroll
         window.addEventListener("scroll", runOnScroll);
 
@@ -67,7 +67,8 @@ chrome.storage.local.get("whitelist", function (returnedStorage) {
             var evtobj = window.event ? event : e
             if (evtobj.keyCode == 82 && evtobj.altKey) {
                 console.log("Alt + R");
-                evaluateContainers('ml5');
+                // evaluateContainers('ml5');
+                evaluateContainers('ocr');
             }
         }
         document.onkeydown = KeyPress;
@@ -89,7 +90,8 @@ var runOnScroll = function (evt) {
         //     evaluateContainers('ml5');
         //     lastPosition = scrollTop;
         // }
-        evaluateContainers('ml5');
+        // evaluateContainers('ml5');
+        evaluateContainers('ocr');
     }, 150);
 };
 
